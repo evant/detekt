8225664713ee4f6edef7abf2b77f171b3a79c0c2
Brais <braisgabin@gmail.com>
2021-10-09 15:33:29 +0200

    Add ContextBinding to FunctionSuppressor

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/suppressors/Suppressors.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/suppressors/Suppressors.kt
@@ -3,17 +3,18 @@ package io.gitlab.arturbosch.detekt.core.suppressors
 import io.gitlab.arturbosch.detekt.api.BaseRule
 import io.gitlab.arturbosch.detekt.api.ConfigAware
 import io.gitlab.arturbosch.detekt.api.Finding
+import org.jetbrains.kotlin.resolve.BindingContext
 
 /**
  * Given a Finding it decides if it should be suppressed (`true`) or not (`false`)
  */
 typealias Suppressor = (Finding) -> Boolean
 
-internal fun getSuppressors(rule: BaseRule): List<Suppressor> {
+internal fun getSuppressors(rule: BaseRule, bindingContext: BindingContext): List<Suppressor> {
     return if (rule is ConfigAware) {
         listOfNotNull(
             annotationSuppressorFactory(rule),
-            functionSuppressorFactory(rule),
+            functionSuppressorFactory(rule, bindingContext),
         )
     } else {
         emptyList()
